2e58fbc96b3f06748ae50a600d20c1fc488a485a
Olga Ukolova <uk0lova@yandex.ru>
2018-12-09 12:31:58 +0300

    задание 2
    добавлен класс Point, реализован вывод расстояния между 2мя точками на экран с помощью функции print, расстояние между 2мя точками вычисляется в методе класса Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,18 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(3, 4);
+        Point p3 = new Point(5, 6);
+
+        print(p1,p2);
+        print(p3,p1);
+    }
+
+    public static void print(Point p1,Point p2){
+        System.out.println("Расстояние между точкой Х с координатами ("
+                + p1.x + "," + p1.y + ") и Y с координатами (" + p2.x + "," + p2.y + ") = " + Point.distance(p1, p2));
     }
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+    double x;
+    double y;
+
+    Point(double x, double y){
+        this.x=x;
+        this.y=y;
+    }
+
+    public static double distance(Point p1, Point p2){
+        return Math.sqrt(Math.pow((p2.x-p1.x),2)+Math.pow((p2.y-p1.y),2));}
+}
