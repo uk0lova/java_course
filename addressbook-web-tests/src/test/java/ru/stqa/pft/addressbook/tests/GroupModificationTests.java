d7e68ee2e7ae52f07660574c3d5b26d9702f09e4
Olga Ukolova <uk0lova@yandex.ru>
2018-12-17 18:25:16 +0300

    Задание 6.  Модификация групп
    добавлен класс GroupModificationTests, реализованы дополнительные методы в GroupHelper

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.addressbook.tests;
+
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+    @Test
+    public void testGroupModification(){
+        app.getNavigationHelper().goToGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupFrom(new GroupData("test4", "test5", "test6"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
