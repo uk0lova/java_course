65c988120ddfe80db7a8eecc49b23c336f1cd3ac
Olga Ukolova <uk0lova@yandex.ru>
2018-12-17 17:39:59 +0300

    Задание 5 работа с группами
    Реализована двузуровневая архитектура для работы с группами

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase {
+
+    @Test
+    public void testGroupDeletion() throws Exception {
+        app.getNavigationHelper().goToGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().deleteSelectedGroups();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
