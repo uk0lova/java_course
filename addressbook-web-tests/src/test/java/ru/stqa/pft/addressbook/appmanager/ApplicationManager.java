036e33acfa9e0f015edba7cc2cab14cd8a212ff9
Olga Ukolova <uk0lova@yandex.ru>
2018-12-17 23:30:57 +0300

    Задание 7 Запуск тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,10 +14,17 @@ public class ApplicationManager {
     private NavigationHelper navigationHelper;
     private SessionHelper sessionHelper;
     private ContactHelper contactHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        //wd = new ChromeDriver();
-        wd = new FirefoxDriver();
+        if (browser.equals(BrowserType.FIREFOX)){ wd = new FirefoxDriver();}
+        else if (browser.equals(BrowserType.CHROME)){ wd = new ChromeDriver();}
+        else if (browser.equals(BrowserType.IE)){ wd = new InternetExplorerDriver();}
+
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/index.php");
         groupHelper = new GroupHelper(wd);
