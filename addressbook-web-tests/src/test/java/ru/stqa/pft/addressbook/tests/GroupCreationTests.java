532bb64e7ccb9b420a1b2c4619a5879ea3a7d48a
Olga Ukolova <uk0lova@yandex.ru>
2019-01-05 12:46:24 +0300

    Задание 9.
    Реализованы проверки путём сравнения списков в тестах для создания, удаления и модификации групп.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,14 +1,29 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
 
 public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().goToGroupPage();
-        app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+        List<GroupData> before=app.getGroupHelper().getGroupList();
+        GroupData group=new GroupData("test1", "test2", "test3");
+        app.getGroupHelper().createGroup(group);
+        List<GroupData> after=app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(),before.size()+1);
+        before.add(group);
+
+        Comparator<? super GroupData> byId = (g1,g2)->Integer.compare(g1.getId(),g2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before,after);
     }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,8 +1,13 @@
 package ru.stqa.pft.addressbook.tests;
 
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
 
 public class GroupModificationTests extends TestBase {
 
@@ -12,10 +17,22 @@ public class GroupModificationTests extends TestBase {
         if(!app.getGroupHelper().isThereAGroup()){
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
-        app.getGroupHelper().selectGroup();
+        List<GroupData> before=app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupFrom(new GroupData("test4", "test5", "test6"));
+        GroupData group=new GroupData(before.get(before.size() - 1).getId(), "test4", "test5", "test6");
+        app.getGroupHelper().fillGroupFrom(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
+        List<GroupData> after=app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(),before.size());
+
+        before.remove(before.size()-1);
+        before.add(group);
+        Comparator<? super GroupData> byId = (g1,g2)->Integer.compare(g1.getId(),g2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before,after);
+
     }
 }
