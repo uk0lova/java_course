678cb7d181e792c5b3471935de9af09c9f0f4936
Olga Ukolova <uk0lova@yandex.ru>
2019-01-08 22:05:02 +0300

    Задание 11.
    В методе fillContactForm добавлено заполнение полей телефонов и емэйлов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -23,6 +23,8 @@ public class ContactHelper extends HelperBase {
         type(By.name("company"), contactData.getCompanyName());
         type(By.name("address"), contactData.getAddress());
         type(By.name("email"), contactData.getEmail());
+        type(By.name("email2"), contactData.getEmail2());
+        type(By.name("email3"), contactData.getEmail3());
         type(By.name("homepage"), contactData.getHomePage());
 
         click(By.name("bday"));
@@ -32,6 +34,9 @@ public class ContactHelper extends HelperBase {
         selectValue(By.name("bmonth"), contactData.getBMonth());
 
         type(By.name("byear"), contactData.getBYear());
+        type(By.name("home"), contactData.getHomePhone());
+        type(By.name("mobile"), contactData.getMobilePhone());
+        type(By.name("work"), contactData.getWorkPhone());
     }
 
     public void deleteSelectedContacts() {
@@ -104,10 +109,11 @@ public class ContactHelper extends HelperBase {
             int id = Integer.parseInt(cells.get(0).findElement(By.tagName("input")).getAttribute("id"));
             String lName = cells.get(1).getText();
             String fName = cells.get(2).getText();
+            String address=cells.get(3).getText();
+            String allEmails = cells.get(4).getText();
             String allPhones = cells.get(5).getText();
-
             ContactData contact = new ContactData().withId(id).withFirstName(fName).withLastName(lName)
-                    .withAllPhones(allPhones);
+                    .withAddress(address).withAllEmails(allEmails).withAllPhones(allPhones);
             contactCache.add(contact);
         }
         return new Contacts(contactCache);
@@ -117,12 +123,17 @@ public class ContactHelper extends HelperBase {
         initContactModificationById(contact.getId());
         String fName=wd.findElement(By.name("firstname")).getAttribute("value");
         String lName=wd.findElement(By.name("lastname")).getAttribute("value");
+        String address=wd.findElement(By.name("address")).getAttribute("value");
+        String email=wd.findElement(By.name("email")).getAttribute("value");
+        String email2=wd.findElement(By.name("email2")).getAttribute("value");
+        String email3=wd.findElement(By.name("email3")).getAttribute("value");
         String home=wd.findElement(By.name("home")).getAttribute("value");
         String mobile=wd.findElement(By.name("mobile")).getAttribute("value");
         String work=wd.findElement(By.name("work")).getAttribute("value");
         wd.navigate().back();
         return new ContactData()
                 .withId(contact.getId()).withFirstName(fName).withLastName(lName)
+                .withAddress(address).withEmail(email).withEmail2(email2).withEmail3(email3)
                 .withHomePhone(home).withMobilePhone(mobile).withWorkPhone(work);
     }
 }
