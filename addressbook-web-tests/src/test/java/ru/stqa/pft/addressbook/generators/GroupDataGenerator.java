413b88d8310c6a4d494851ed993bb9a5c473769f
Olga Ukolova <uk0lova@yandex.ru>
2019-01-09 14:05:58 +0300

    Реализована загрузка данных из файла для теста GroupCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -15,18 +15,18 @@ import java.util.List;
 
 public class GroupDataGenerator {
 
-    @Parameter(names="-c", description="Group count")
+    @Parameter(names = "-c", description = "Group count")
     public int count;
 
-    @Parameter(names="-f",description = "Target file")
+    @Parameter(names = "-f", description = "Target file")
     public String file;
 
     public static void main(String[] args) throws IOException {
         GroupDataGenerator generator = new GroupDataGenerator();
-        JCommander jCommander=  new JCommander(generator);
+        JCommander jCommander = new JCommander(generator);
         try {
             jCommander.parse(args);
-        } catch(ParameterException ex){
+        } catch (ParameterException ex) {
             jCommander.usage();
             return;
         }
@@ -34,25 +34,25 @@ public class GroupDataGenerator {
     }
 
     private void run() throws IOException {
-        List<GroupData> groups=generateGroups(count);
-        save(groups,new File(file));
+        List<GroupData> groups = generateGroups(count);
+        save(groups, new File(file));
 
     }
 
     private void save(List<GroupData> groups, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
-        Writer writer=new FileWriter(file);
-        for (GroupData group:groups){
-            writer.write(String.format("%s;%s;%s\n",group.getName(),group.getHeader(),group.getFooter()));
+        Writer writer = new FileWriter(file);
+        for (GroupData group : groups) {
+            writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
         }
         writer.close();
     }
 
     private List<GroupData> generateGroups(int count) {
-        List<GroupData> groups= new ArrayList<GroupData>();
-        for(int i=0; i<count; i++){
-            groups.add(new GroupData().withName(String.format("test %s",i))
-            .withHeader(String.format("header %s",i)).withFooter(String.format("footer %s",i)));
+        List<GroupData> groups = new ArrayList<GroupData>();
+        for (int i = 0; i < count; i++) {
+            groups.add(new GroupData().withName(String.format("test %s", i))
+                    .withHeader(String.format("header %s", i)).withFooter(String.format("footer %s", i)));
         }
         return groups;
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -14,25 +15,29 @@ import static org.hamcrest.MatcherAssert.*;
 public class GroupCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validGroups(){
-        List<Object[]> list =new ArrayList<Object[]>();
-        list.add(new Object[] {new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
-        list.add(new Object[] {new GroupData().withName("test2").withHeader("header2").withFooter("footer2")});
-        list.add(new Object[] {new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
+    public Iterator<Object[]> validGroups() throws IOException {
+        List<Object[]> list = new ArrayList<Object[]>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] split = line.split(";");
+            list.add(new Object[]{new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+            line = reader.readLine();
+        }
         return list.iterator();
     }
 
     @Test(dataProvider = "validGroups")
     public void testGroupCreation(GroupData group) throws Exception {
         app.goTo().groupPage();
-        Groups before=app.group().all();
+        Groups before = app.group().all();
 
-         app.group().create(group);
+        app.group().create(group);
 
-        assertThat(app.group().count(),equalTo(before.size()+1));
-        Groups after=app.group().all();
+        assertThat(app.group().count(), equalTo(before.size() + 1));
+        Groups after = app.group().all();
         assertThat(after, equalTo(
-                before.withAdded(group.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
+                before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
 
 }
