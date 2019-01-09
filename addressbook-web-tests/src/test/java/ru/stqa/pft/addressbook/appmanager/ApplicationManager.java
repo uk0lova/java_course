5312371128be6b8a4bc22ae07b5043384d728b4c
Olga Ukolova <uk0lova@yandex.ru>
2019-01-09 22:00:21 +0300

    Задание 14.
    Реализована загрузка информации о тестовом стенде из конфигурационного файла

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,9 +6,14 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+    private final Properties properties;
     private WebDriver wd;
     private GroupHelper groupHelper;
     private NavigationHelper navigationHelper;
@@ -18,21 +23,29 @@ public class ApplicationManager {
 
     public ApplicationManager(String browser) {
         this.browser = browser;
+        properties = new Properties();
     }
 
-    public void init() {
-        if (browser.equals(BrowserType.FIREFOX)){ wd = new FirefoxDriver();}
-        else if (browser.equals(BrowserType.CHROME)){ wd = new ChromeDriver();}
-        else if (browser.equals(BrowserType.IE)){ wd = new InternetExplorerDriver();}
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(String.format("src/test/resources/%s.properties", target)));
+
+        if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IE)) {
+            wd = new InternetExplorerDriver();
+        }
 
         wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/index.php");
+        wd.get(properties.getProperty("web.baseUrl"));
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
-        sessionHelper=new SessionHelper(wd);
-        contactHelper=new ContactHelper(wd);
+        sessionHelper = new SessionHelper(wd);
+        contactHelper = new ContactHelper(wd);
 
-        sessionHelper.login("admin", "secret");
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
 
     public void stop() {
