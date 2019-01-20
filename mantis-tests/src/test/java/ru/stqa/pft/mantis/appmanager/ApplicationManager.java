fc507919e91cbdf6a49f59e544488c2f2290c4e8
Olga Ukolova <uk0lova@yandex.ru>
2019-01-20 22:19:18 +0300

    добавлен тест testLogin() и класс HttpSession

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -42,4 +42,11 @@ public class ApplicationManager {
         wd.quit();
     }
 
-   }
+    public HttpSession newSession(){
+        return new HttpSession(this);
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+}
