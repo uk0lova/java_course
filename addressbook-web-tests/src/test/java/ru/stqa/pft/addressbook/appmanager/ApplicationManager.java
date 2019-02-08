306ba4568bc37eb0123b9a8d84392af9a210a847
Olga Ukolova <uk0lova@yandex.ru>
2019-02-08 08:31:35 +0300

    указан драйвер в webdriver.chrome.driver

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -33,7 +33,7 @@ public class ApplicationManager {
 
     public void init() throws IOException {
 
-        System.setProperty("webdriver.chrome.driver", "./");
+        System.setProperty("webdriver.chrome.driver", "./chromedriver");
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(String.format("src/test/resources/%s.properties", target)));
 
